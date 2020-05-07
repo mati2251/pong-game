c69d0a4ad5cfbb00f16cda153a04b3865e092667
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-05-07 11:22:52 +0200

    Add Ball and add footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,41 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-console.log(canvas)
+class Ball {
+	constructor(x,y) {
+		this.x=x;
+		this.y=y;
+		this.drawBall();
+	}
+
+	setPosition(x,y){
+		this.x=x;
+		this.y=y;
+	}
+
+	drawBall(){
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, 20, 0, 2*Math.PI)
+		ctx.strokeStyle = "#FFFFFF"
+		ctx.lineWidth = 4;
+		ctx.stroke()
+	}
+
+}
+
 const setCanvasDimension = () => {
 	canvas.width = window.innerWidth-30;
-	canvas.height = window.innerHeight-60;
+	canvas.height = window.innerHeight-150;
 };
 
 setCanvasDimension()
+
+document.getElementById("start").onclick = (() => {
+	const ball = new Ball(canvas.width/2, 50)
+})
+
+window.addEventListener("resize", () => {
+	setCanvasDimension()
+})
+
