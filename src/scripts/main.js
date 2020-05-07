63fab1dec9a0e1581757ffa0a0cf84306ad0eab4
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-05-07 15:32:17 +0200

    Add paddle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,40 +2,89 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 class Ball {
-	constructor(x,y) {
-		this.x=x;
-		this.y=y;
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
 		this.drawBall();
 	}
 
-	setPosition(x,y){
-		this.x=x;
-		this.y=y;
+	getPosition = () => {
+		return {x: this.x, y: this.y}
 	}
 
-	drawBall(){
-		ctx.closePath();
+	setPosition = (x, y) => {
+		this.x = x;
+		this.y = y;
+	}
+
+	startBall = () => {
+	}
+
+	drawBall = () => {
 		ctx.beginPath();
-		ctx.arc(this.x, this.y, 20, 0, 2*Math.PI)
+		ctx.arc(this.x, this.y, 20, 0, 2 * Math.PI)
 		ctx.strokeStyle = "#FFFFFF"
 		ctx.lineWidth = 4;
-		ctx.stroke()
+		ctx.stroke();
+		ctx.closePath();
+	}
+}
+
+class Paddle {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.drawPaddle();
 	}
 
+	setPosition(x) {
+		this.x = x;
+	}
+
+	cleanPaddle = () => {
+		ctx.clearRect(this.x - 4, this.y - 4, 208, 28);
+	}
+
+	getPosition = () => {
+		return {x: this.x, y: this.y}
+	}
+
+	changePaddlePositionWithMouse = (event) => {
+		const positionX = event.pageX - 104;
+		console.log(positionX);
+		console.log(canvas.width);
+		this.cleanPaddle();
+		this.setPosition(positionX);
+		this.drawPaddle();
+	}
+
+	drawPaddle = () => {
+		ctx.beginPath();
+		ctx.strokeStyle = "#FFFFFF"
+		ctx.lineWidth = 4;
+		ctx.strokeRect(this.x, this.y, 200, 20)
+		ctx.closePath();
+	}
 }
 
 const setCanvasDimension = () => {
-	canvas.width = window.innerWidth-30;
-	canvas.height = window.innerHeight-150;
+	canvas.width = window.innerWidth - 30;
+	canvas.height = window.innerHeight - 150;
 };
 
 setCanvasDimension()
 
 document.getElementById("start").onclick = (() => {
-	const ball = new Ball(canvas.width/2, 50)
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.className = "border"
+	const ball = new Ball(canvas.width / 2, 50)
+	const paddle = new Paddle(canvas.width / 2 - 100, canvas.height - 50)
+	document.onmousemove = (e) => paddle.changePaddlePositionWithMouse(e)
 })
 
 window.addEventListener("resize", () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	document.onmousemove = (e) => undefined
 	setCanvasDimension()
 })
 
